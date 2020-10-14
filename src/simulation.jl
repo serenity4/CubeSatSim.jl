924c4fdf49547aea1db61ca560e9120d80c6bba9
serenity4 <cedric.bel@hotmail.fr>
2020-10-14 15:25:25 +0200

    Add Euler equation + drag + gravity

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,7 +1,10 @@
 using ModelingToolkit
-using OrdinaryDiffEq
+using OrdinaryDiffEq, DiffEqCallbacks
+using BenchmarkTools
+using LinearAlgebra
+using DataFrames
 
-@parameters t
+@parameters t, I[1:3, 1:3], Iinv[1:3, 1:3], g[1:3], m, α
 @variables q[1:4](t) x[1:3](t) ω[1:3](t) v[1:3](t)
 @derivatives D'~t
 
@@ -29,19 +32,57 @@ function dynamics(q, x, ω, v)
     P = transition_matrix(q)
     vcat(
         D.(q) .~ quaternion_derivative(q, ω),
-        D.(x) .~ v,
-        D.(ω) .~ 0.,
-        D.(v) .~ 0.
+        D.(x) .~ P * v, # world velocity
+        D.(ω) .~ Iinv * cross(I * ω, ω),
+        D.(v) .~ 1/m * (m * P' * g - α .* v) # body acceleration
     )
 end
+
+function postprocess(u) # dynamics_f does not work
+    q, x, ω, v = u[1:4, end], u[5:7, end], u[8:10, end], u[11:13, end]
+    dynamics_f(q, x, ω, v)
+end
+
+get_values(eqs) = getproperty.(getproperty.(eqs, :rhs), :value)
+function extract_result(u)
+    syms = [:q1, :q2, :q3, :q4, :x, :y, :z, :ωx, :ωy, :ωz, :vx, :vy, :vz]
+    df = DataFrame(repeat([Float64], 13), syms)
+    for i ∈ 1:size(u)[2]
+        push!(df, Dict(syms .=> u[:, i]))
+    end
+    df
+end
+
+function quaternion_norm_callback(resid, u, p, t)
+    resid[1] = @views norm(u[1:4])^2 - 1
+    resid[2:13] .= 0
+end
+
+quaternion_manifold_proj = ManifoldProjection(quaternion_norm_callback)
 
 u0 = vcat(
     q .=> [1., 0, 0, 0],
     x .=> 0.,
-    ω .=> [0., 0., 0.2],
-    v .=> [1., 0, 0]
+    ω .=> [0.1, 0.15, 0.2],
+    v .=> [1., 0, 0],
 )
-tspan = (0.0, 100.0)
+
+p = vcat(
+    (I .=> Diagonal([0.25, 0.2, 0.1]))...,
+    # (Iinv .=> inv(I))...
+    (Iinv .=> Diagonal([4, 5., 10.]))...,
+    g .=> [0., 0., -9.81],
+    m => 3.,
+    α => 0.01
+)
+
+tspan = (0.0, 10000.0)
 sys = ODESystem(dynamics(q, x, ω, v))
-prob = ODEProblem(sys, u0, tspan)
-solve(prob, Tsit5())
+prob = ODEProblem(sys, u0, tspan, p)
+
+u = solve(prob, Vern7(), save_everystep=false, callback=quaternion_manifold_proj)
+df = extract_result(u)
+
+# dynamics_f = build_function(dynamics(q, x, ω, v), q, x, ω, v; expression=Val{false})
+# postprocess(u)
+
